1e5017385c83de07dc4a31b83590a3d52cfe404a
Christoph Herr <info@christophherr.com>
2016-10-19 03:37:58 +0000

    Setting up MongoDB

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 var express = require('express');
-
+var fs = require('fs');
+var path = require('path');
+var mongoose = require('mongoose');
 
 var app = express();
 
+var mongoPath = process.env.MONGOLAB_URI || "mongodb://localhost:27017/"
+
 app.set('port', process.env.PORT);
 
+mongoose.connect(mongoPath);
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
 
 
 app.listen(app.get('port'), function() {
