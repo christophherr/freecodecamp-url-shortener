c40dfaa39821d7ae79634d3ba09bcf5cf4b529af
Christoph Herr <info@christophherr.com>
2016-10-19 03:57:46 +0000

    Adds placeholder text

--- server.js
+++ server.js
@@ -2,12 +2,21 @@ var express = require('express');
 var fs = require('fs');
 var path = require('path');
 var mongoose = require('mongoose');
+var validurl = require('valid-url');
 
 var app = express();
 
 var mongoPath = process.env.MONGOLAB_URI || "mongodb://localhost:27017/"
 
 app.set('port', process.env.PORT);
+
+app.get('/', function (req, res) {
+  res.send("Freecodecamp URL Shortener Microservice Coming Soon")
+})
+
+app.use(function(req, res){
+    res.sendStatus(404); 
+});
 
 mongoose.connect(mongoPath);
 
